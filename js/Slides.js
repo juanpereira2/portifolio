de9cf2b3190d55ae0b021ace31698b5eeffabb83
juanpereira2 <juanpereira74@hotmail.com>
2020-05-22 11:46:32 -0300

    added comehome

--- js/Slides.js
+++ js/Slides.js
@@ -1,21 +1,23 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = [1, 1];
+var slideId = ["mySlides1", "mySlides2"];
+showDivs(1, 0);
+showDivs(1, 1);
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
+function plusDivs(n, no) {
+  showDivs((slideIndex[no] += n), no);
 }
 
-function showDivs(n) {
+function showDivs(n, no) {
   var i;
-  var x = document.getElementsByClassName("mySlides rounded mb-5");
+  var x = document.getElementsByClassName(slideId[no]);
   if (n > x.length) {
-    slideIndex = 1;
+    slideIndex[no] = 1;
   }
   if (n < 1) {
-    slideIndex = x.length;
+    slideIndex[no] = x.length;
   }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex - 1].style.display = "block";
+  x[slideIndex[no] - 1].style.display = "block";
 }
